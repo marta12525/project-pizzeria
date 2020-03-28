6a1aebc221cffa079e182b835e5d79df51cd9ad2
Marta Bednarz <martab2525@gmail.com>
2020-03-28 14:58:56 +0100

    add initAccordion method

--- src/js/script.js
+++ src/js/script.js
@@ -53,15 +53,104 @@
   };
 
   const app = {
-    init: function(){
+    initMenu: function () {
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
+      //const testProduct = new Product();
+      //console.log('test Product:', testProduct);
+      for(let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    initData: function () {
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
+    init: function () {
       const thisApp = this;
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData ();
+      thisApp.initMenu ();
     },
   };
 
+  class Product {
+    constructor (id, data) {
+      const thisProduct = this;
+
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+      console.log('new Product:', thisProduct);
+
+      thisProduct.initAccordion();
+    }
+
+    renderInMenu () {
+      const thisProduct = this;
+
+      /* generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      console.log(generatedHTML);
+
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
+
+    }
+
+    initAccordion () {
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      //console.log(clickableTrigger);
+  
+      /* START: click event listener to trigger */
+      clickableTrigger.addEventListener('click', function (event) {
+
+        /* prevent default action for event */
+        event.preventDefault();
+  
+        /* toggle active class on element of thisProduct */
+        thisProduct.element.classList.toggle('active');
+  
+        /* find all active products */
+        const activeProducts = document.querySelectorAll('.active');
+  
+        /* START LOOP: for each active product */
+        for(let activeProduct of activeProducts) {
+
+          /* START: if the active product isn't the element of thisProduct */
+          if (activeProduct !== thisProduct.element) {
+            /* remove class active for the active product */
+            activeProduct.classList.remove('active');
+          /* END: if the active product isn't the element of thisProduct */
+          }
+
+        /* END LOOP: for each active product */
+        }
+
+      /* END: click event listener to trigger */
+      })
+
+    }
+  }
+
   app.init();
+
 }
